30b9902435055e1f619457968dc505ddad80820b
Kevin Hwang <timmyhwang127@gmail.com>
2022-07-10 16:12:03 -0400

    Added minheap

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+
 class MaxHeap:
 
     def __init__(self, heap=[]):
@@ -90,6 +91,100 @@ class MaxHeap:
                 return False
                 
             if right<self.heapSize and self.heap[right]>parent:
+                return False
+            
+            return dfs(left) and dfs(right)
+
+        return dfs(i=0)
+
+class MinHeap:
+
+    def __init__(self, heap=[]):
+
+        self.heap = heap
+        self.heapSize = len(self.heap)
+
+    def find_left(self,i):
+        return 2*i +1
+
+    def find_right(self, i):
+        return (2*i)+2
+
+    def swap(self, i1, i2):
+        tmp = self.heap[i1]
+        self.heap[i1] = self.heap[i2]
+        self.heap[i2] = tmp
+
+    def min_heapify(self, i):
+        
+        if not self.heap:
+            return
+
+        left = self.find_left(i)
+        right = self.find_right(i)
+
+
+        if left<self.heapSize and self.heap[left]<self.heap[i]:
+            smallest = left
+        else:
+            smallest = i
+
+        if right<self.heapSize and self.heap[right]<self.heap[smallest]:
+            smallest = right
+        
+        if smallest!=i:
+            self.swap(i,smallest)
+            self.min_heapify(smallest)
+
+    def build_min_heap(self, arr):
+        n = len(arr)//2 - 1
+
+        self.heap = deepcopy(arr)
+        self.heapSize = len(arr)
+
+        while n>=0:
+            self.min_heapify(n)
+            n-=1
+
+    def find_min(self):
+        if not self.heap:
+            return None
+
+        return self.heap[0]
+    
+    def extract_min(self):
+        mx = self.heap.pop(0)
+        
+        # Heap is now jumbled mess, turn it back to min_heap
+        self.build_min_heap(self.heap)
+
+        return mx
+
+    def edit_node(self, i, val):
+        self.heap[i] = val
+        self.build_min_heap(self.heap)
+
+    def has_mhp(self):
+        print(self.heap)
+
+        def dfs(i):
+            
+            if i>=self.heapSize:
+                return True
+            
+            left = self.find_left(i)
+            right = self.find_right(i)
+            parent = self.heap[i]
+
+
+            print(i,left,right)
+            
+
+            # IF IN HEAP AND LESS THAN PARENT
+            if left<self.heapSize and self.heap[left]<parent:
+                return False
+                
+            if right<self.heapSize and self.heap[right]<parent:
                 return False
             
             return dfs(left) and dfs(right)
